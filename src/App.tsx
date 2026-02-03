b31ade822640ba540d956960becca3245351d4bc
Cahyadi-Prasetyo <cahyadiprasetyo659@gmail.com>
2026-02-03 20:49:58 +0700

    Bongkar Tahap 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,8 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import { LanguageProvider } from './context/LanguageContext';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import ProjectDetail from './pages/ProjectDetail';
-import PageTransition from './components/PageTransition';
-import ScrollProgress from './components/reactbits/ScrollProgress';
-import ScrollToTop from './components/ScrollToTop';
 
-function App() {
-    const location = useLocation();
-
+export default function App() {
     return (
-        <LanguageProvider>
-            <div className="min-h-screen flex flex-col bg-white text-gray-900 font-sans relative">
-                <ScrollToTop />
-                <ScrollProgress />
-                <Navbar />
-                <main className="flex-grow">
-                    <AnimatePresence mode="wait">
-                        <Routes location={location} key={location.pathname}>
-                            <Route path="/" element={<PageTransition><Home /></PageTransition>} />
-                            <Route path="/projects" element={<PageTransition><Projects /></PageTransition>} />
-                            <Route path="/projects/:slug" element={<PageTransition><ProjectDetail /></PageTransition>} />
-                            <Route path="/about" element={<PageTransition><About /></PageTransition>} />
-                            <Route path="/contact" element={<PageTransition><Contact /></PageTransition>} />
-                        </Routes>
-                    </AnimatePresence>
-                </main>
-                <Footer />
-            </div>
-        </LanguageProvider>
-    )
+        <div className="h-screen w-full bg-black text-white flex items-center justify-center">
+            <h1 className="text-2xl font-mono">Booting CahyadiOS...</h1>
+        </div>
+    );
 }
-
-
-export default App
