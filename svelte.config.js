a3c36e3afe46568ce0efcaf6da39681f260e95ac
Cahyadi-Prasetyo <cahyadiprasetyo659@gmail.com>
2026-02-16 22:09:31 +0700

    chore: Remove Svelte project configuration and source files, creating new Vite dependency metadata.

--- svelte.config.js
+++
@@ -1,8 +0,0 @@
-import sveltePreprocess from 'svelte-preprocess'
-
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess(),
-}
